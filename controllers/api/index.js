fed1de6cdf79770e553d8158bef30f3b1880eb1c
jyoon3 <jyoon3@2u.com>
2024-07-15 21:56:10 -0400

    got POST and GET working, dont forget the server next time KEK

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,33 @@
 const router = require("express").Router();
 const Task = require('../../models/Task');
 
+router.get('/', async(req, res) => {
+  try {
+    const allTasks = await Task.findAll();
+
+    res.status(200).json(allTasks);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+})
+
 // localhost:3001/api/
 router.post('/', async (req, res) => {
-  const newTask = Task.create({
-    name: 'Feed Pikachu',
-    description: 'Feed Pikachu he is hungry',
-    status: 'todo'
-  });
+  // const { name, description, status } = req.body; Object Destructuring 
+  try {
+    const newTask = await Task.create({
+      name: req.body.name,
+      description: req.body.description,
+      status: req.body.status
+    })
 
-  res.json(newTask);
+    if(newTask) {
+      res.status(200).json(newTask);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 })
 
 
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const controllers = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(controllers);
 
 sequelize.sync().then(() => {
