f93a05e2fbcea013dc64c036376cbf5a89d687ed
jyoon3 <jyoon3@2u.com>
2024-07-15 21:37:03 -0400

    made the models content and generated tables for db

---
+++ models/Task.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Task extends Model {};
+
+Task.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  }, 
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'task'
+});
+
+module.exports = Task;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 
+// const model = require('./models/Task');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
