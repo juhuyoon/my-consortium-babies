d18bd4ea2017dc5499cba681d085fc412a279251
jyoon3 <jyoon3@2u.com>
2024-07-15 22:08:57 -0400

    handlebars front end with for each loop

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const api = require('./api');
+const Task = require('../models/Task');
 
 // localhost:3001/api
 router.use("/api", api);
+
+router.get('/', async (req, res) => {
+  const allTasks = await Task.findAll();
+  const sanitizedData = allTasks.map((task) => {
+    return task.get({ plain: true});
+  })
+  console.log(allTasks);
+  console.log('=========================')
+  console.log(sanitizedData);
+  // const testData = {
+  //   name: 'test work',
+  //   description: 'work to do',
+  //   status: 'in progress'
+  // };
+  res.render('todo', { sanitizedData });
+})
 
 // localhost:3001/pikachu
 router.get("/pikachu", (req, res) => {
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const controllers = require('./controllers');
+const exphbs = require('express-handlebars');
 // const model = require('./models/Task');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
