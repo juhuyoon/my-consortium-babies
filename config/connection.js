9196bfc877ac55abaa0e02a01d29af62e97e681f
jyoon3 <jyoon3@2u.com>
2024-07-15 21:29:56 -0400

    updated env file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  "todo_app_db",
-  "postgres",
-  "theskyisbluetoday",
+const sequelize = new Sequelize(process.env.PIKACHU_DB, process.env.PIKACHU_USER,
+ process.env.PIKACHU_PW,
   {
     host: 'localhost',
     dialect: 'postgres'
